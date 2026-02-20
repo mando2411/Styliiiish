1d0ba2687fa59535772c1d9327e0d207f74d7237
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-20 17:14:04 +0200

    Batch upload auto retry #11 (2026-02-20 17:14:04)

---
+++ wp-content/plugins/fable-extra/inc/themes/shopire/custom-style.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Enqueue User Custom styles.
+ */
+if( ! function_exists( 'desert_shopire_user_custom_style' ) ):
+    function desert_shopire_user_custom_style() {
+
+		$shopire_print_style = '';
+		$fable_axtra_activated_theme = wp_get_theme(); // gets the current theme
+		
+		/*=========================================
+		Slider 
+		=========================================*/
+		if( 'BuyCart' == $fable_axtra_activated_theme->name){
+			$opacity='0.7';
+		}else{
+			$opacity='0';
+		}	
+		$shopire_slider_overlay 	= get_theme_mod('shopire_slider_overlay','#000000');
+		$shopire_slider_opacity	= get_theme_mod('shopire_slider_opacity',$opacity);
+		list($color1, $color2, $color3) = sscanf($shopire_slider_overlay, "#%02x%02x%02x");
+		$shopire_print_style .=".wf_slider .wf_slider-wrapper, .wf_slider .wf_slider-item img+.wf_slider-wrapper {
+			background-color: rgba($color1, $color2, $color3, $shopire_slider_opacity);
+		}\n";
+				
+        wp_add_inline_style( 'shopire-style', $shopire_print_style );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'desert_shopire_user_custom_style' );
