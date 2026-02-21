1fe4589e385ab2caf1c2880f2045df5ddcfec581
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-21 13:48:59 +0200

    Auto update 2026-02-21 13:48

--- index.php
+++ index.php
@@ -40,6 +40,42 @@ if (!$send_to_laravel) {
 }
 
 if ($send_to_laravel) {
+    $laravel_public = __DIR__ . '/laravel_home/public';
+    $requested_file = realpath($laravel_public . $request_uri);
+
+    if (
+        $requested_file !== false &&
+        strpos($requested_file, realpath($laravel_public)) === 0 &&
+        is_file($requested_file)
+    ) {
+        $ext = strtolower(pathinfo($requested_file, PATHINFO_EXTENSION));
+        $mime_types = [
+            'png' => 'image/png',
+            'jpg' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'svg' => 'image/svg+xml',
+            'ico' => 'image/x-icon',
+            'webp' => 'image/webp',
+            'css' => 'text/css; charset=UTF-8',
+            'js' => 'application/javascript; charset=UTF-8',
+            'json' => 'application/json; charset=UTF-8',
+            'txt' => 'text/plain; charset=UTF-8',
+            'woff' => 'font/woff',
+            'woff2' => 'font/woff2',
+            'ttf' => 'font/ttf',
+            'map' => 'application/json; charset=UTF-8',
+        ];
+
+        if (isset($mime_types[$ext])) {
+            header('Content-Type: ' . $mime_types[$ext]);
+        }
+
+        header('Cache-Control: public, max-age=604800');
+        readfile($requested_file);
+        exit;
+    }
+
     require __DIR__ . '/laravel_home/public/index.php';
     exit;
 }
