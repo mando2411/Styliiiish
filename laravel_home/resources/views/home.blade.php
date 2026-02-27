4e98d0714b505446e34953c2c6a28e14ea60327c
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-27 23:58:51 +0200

    Auto update 2026-02-27 23:58

--- laravel_home/resources/views/home.blade.php
+++ laravel_home/resources/views/home.blade.php
@@ -2917,7 +2917,7 @@
 
             .review-shot img {
                 width: 100%;
-                height: 130px;
+                height: 100px;
                 aspect-ratio: auto;
                 max-height: none;
                 border-radius: 9px;
