81ad2d813933140a205eca9e6d150e960b947304
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-28 07:10:40 +0200

    Auto update 2026-02-28 07:10

--- laravel_home/resources/views/partials/shared-seo-meta.blade.php
+++ laravel_home/resources/views/partials/shared-seo-meta.blade.php
@@ -53,6 +53,16 @@
 
     $seoLocaleCode = $isEnglishLocale ? 'en_US' : 'ar_EG';
     $seoAltLocaleCode = $isEnglishLocale ? 'ar_EG' : 'en_US';
+    $seoLanguageCode = $isEnglishLocale ? 'en' : 'ar';
+    $seoOrganizationName = 'Styliiiish';
+    $seoOrganizationDescription = $isEnglishLocale
+        ? 'Styliiiish is an online fashion platform for evening, bridal, and engagement dresses with trusted support and Egypt-wide delivery.'
+        : 'ستايلش منصة أزياء إلكترونية لفساتين السهرة والزفاف والخطوبة مع دعم موثوق وتوصيل داخل مصر.';
+    $seoSameAs = [
+        'https://www.facebook.com/Styliiish.Egypt/',
+        'https://www.instagram.com/styliiiish.egypt/',
+        'https://g.page/styliish',
+    ];
 @endphp
 <meta name="keywords" content="{{ $seoKeywordsValue }}">
 <meta name="author" content="Styliiiish">
@@ -64,15 +74,58 @@
 <script type="application/ld+json">
 {!! json_encode([
     '@context' => 'https://schema.org',
+    '@type' => 'Organization',
+    'name' => $seoOrganizationName,
+    'url' => $seoBaseUrl,
+    'logo' => $seoImageValue,
+    'description' => $seoOrganizationDescription,
+    'sameAs' => $seoSameAs,
+    'areaServed' => [
+        ['@type' => 'Country', 'name' => 'Egypt'],
+        ['@type' => 'Country', 'name' => 'Switzerland'],
+    ],
+], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}
+</script>
+<script type="application/ld+json">
+{!! json_encode([
+    '@context' => 'https://schema.org',
+    '@type' => 'WebSite',
+    'name' => $seoOrganizationName,
+    'url' => $seoBaseUrl,
+    'inLanguage' => [$seoLanguageCode, $isEnglishLocale ? 'ar' : 'en'],
+    'publisher' => [
+        '@type' => 'Organization',
+        'name' => $seoOrganizationName,
+        'url' => $seoBaseUrl,
+    ],
+], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}
+</script>
+<script type="application/ld+json">
+{!! json_encode([
+    '@context' => 'https://schema.org',
     '@type' => 'WebPage',
     'name' => $seoTitleValue,
     'description' => $seoDescriptionValue,
     'url' => $seoUrlValue,
-    'inLanguage' => $isEnglishLocale ? 'en' : 'ar',
+    'inLanguage' => $seoLanguageCode,
     'isPartOf' => [
         '@type' => 'WebSite',
-        'name' => 'Styliiiish',
+        'name' => $seoOrganizationName,
         'url' => $seoBaseUrl,
+    ],
+    'about' => [
+        '@type' => 'Organization',
+        'name' => $seoOrganizationName,
+        'url' => $seoBaseUrl,
+    ],
+    'publisher' => [
+        '@type' => 'Organization',
+        'name' => $seoOrganizationName,
+        'url' => $seoBaseUrl,
+        'logo' => [
+            '@type' => 'ImageObject',
+            'url' => $seoImageValue,
+        ],
     ],
 ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) !!}
 </script>
