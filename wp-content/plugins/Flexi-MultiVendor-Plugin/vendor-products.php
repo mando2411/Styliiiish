c032673d63c17fd35d9c1ba83d39dbd9814dafac
mando2411 <10312023101163@stud.cu.edu.eg>
2026-03-03 08:52:42 +0200

    Auto update 2026-03-03 08:52

--- wp-content/plugins/Flexi-MultiVendor-Plugin/vendor-products.php
+++ wp-content/plugins/Flexi-MultiVendor-Plugin/vendor-products.php
@@ -465,7 +465,7 @@ $(document).on('click', '.sty-reject', function(e){
     const wfRenderVendorListError = (message) => {
         $(".sty-vendor-list").html(`<div class="sty-no-items" translate="no">${message}</div>`);
     };
-/*
+
 $(document).on("click", ".vp-filter-btn", function (e) {
     e.preventDefault();
 
@@ -509,7 +509,7 @@ $(document).on("click", ".vp-filter-btn", function (e) {
 
     }, 'json');
 });
-*/
+
 						  
 						  
 						  
@@ -533,7 +533,7 @@ $(document).on("click", ".vp-filter-btn", function (e) {
 						  
 						  
 
-});					  
+				  
 </script>
 
     <?php
